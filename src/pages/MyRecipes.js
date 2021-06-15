309a5a4a8c0e1a8c2a84921fc9ad445a12b12556
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-06-14 22:28:36 -0400

    fixed Pauls stuff w/ Paul and team

--- src/pages/MyRecipes.js
+++ src/pages/MyRecipes.js
@@ -1,38 +1,42 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import {Link} from 'react-router-dom';
-const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
 
 const MyRecipes = (props) => {
-  const [userRecipes, setUserRecipes] = useState([]);  
+  const [userRecipes, setUserRecipes] = useState([])
 
   useEffect(() => {
-    axios.get(`${REACT_APP_SERVER_URL}/api/users/recipes`)
-    .then(response => {
-      console.log(response.data);
-      setUserRecipes(response.data);
-    }).catch(error => {
-      console.log('------------ MYRECIPE ERROR ------------')
-      console.log(error);
-    })
-  }, []);
+    axios
+      .get(`${REACT_APP_SERVER_URL}/api/users/recipes`)
+      .then((response) => {
+        console.log(response.data)
+        setUserRecipes(response.data)
+      })
+      .catch((error) => {
+        console.log('------------ MYRECIPE ERROR ------------')
+        console.log(error)
+      })
+  }, [])
 
   if (userRecipes) {
     var recipesList = userRecipes.map((recipe, index) => {
       let location = {
         pathname: '/editrecipe',
-        state: recipe
-      };
+        state: recipe,
+      }
       return (
-        <article className="meal" key={index}>
-          <div className="img-container">
+        <article className='meal' key={index}>
+          <div className='img-container'>
             <img src={recipe.thumbnail} alt={recipe.name} />
           </div>
-          <div className="meal-footer">
+          <div className='meal-footer'>
             <h3>{recipe.name}</h3>
             <p>{recipe.category}</p>
-            <Link to={location} className="btn btn-primary btn-details"> Details </Link>
+            <Link to={location} className='btn btn-primary btn-details'>
+              {' '}
+              Details{' '}
+            </Link>
           </div>
         </article>
       )
@@ -43,24 +47,14 @@ const MyRecipes = (props) => {
 
   return (
     <div>
-      <h1>My Saved Recipes</h1>
-      <p>
-        Write a function that grabs data from "mongoDB" and uses the collection
-        "MyRecipes" to list all the users saved recipes on this page. Link this page to 'RecipeInfo.js' to see ingredients in recipe.
-      </p>
-      <li>User should have a way to add recipes</li>
-      <li>User should have a way to edit recipes</li>
-      <li>User should be able to remove recipes</li>
-      <li>User should be able to view all ingredients in recipe</li>
       <section>
-        <h2 className='section-title'> My Recipes </h2>
+        <h1 className='section-title'> My Recipes </h1>
         <div className='meals-center'>
-        {userRecipes.length ? recipesList : <p> ...Loading... </p>}
+          {userRecipes.length ? recipesList : <p> ...Loading... </p>}
         </div>
       </section>
     </div>
   )
 }
 
-
-export default MyRecipes;
+export default MyRecipes
